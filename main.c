c36dabf927d9579eb3d2ff7f31e97ec9de93b0ff
文及信 Văn Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2023-12-11 14:52:41 -0500

    fuck my life

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "sum.h"
+
+int main()
+{
+	int number = sum(10, 10);
+}
---
+++ sum.h
@@ -0,0 +1,13 @@
+//
+// Created by Victor Van on 12/11/2023.
+//
+
+#ifndef C_LEARN_SUM_H
+#define C_LEARN_SUM_H
+
+#endif //C_LEARN_SUM_H
+
+int sum(int x, int y)
+{
+	return x + y;
+}
